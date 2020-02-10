caa3eb7964e38767508840685629c9b6328c13c8
Jurij Waite <jurijlwaite@gmail.com>
2020-02-10 20:13:15 +0100

    Added basic player physics and controls.

--- sketch.js
+++ sketch.js
@@ -1,32 +1,84 @@
+class Player {
+  constructor(r = 20) {
+    this.x = 0.45 * width;
+    this.y = 0.5 * height;
+    this.r = r;
+
+    this.xVel = xVel;
+    this.yVel = 0;
+  }
+
+  show() {
+    // Set coloring properties
+    const yellow = color(220, 220, 100);
+    fill(yellow);
+    noStroke();
+
+    // Draw a circle at the player location
+    circle(this.x, this.y, this.r * 2);
+  }
+
+  update() {
+    this.y += this.yVel;
+    this.yVel += g;
+  }
+}
+
+// Window settings
+const width = 400;
+const height = 600;
+const fps = 60;
+
+const g = 0.35;
+const xVel = 10; // Same as the speed at which the pipes move toward player
+
+const player = new Player();
+
+const coolDownTime = fps / 5; // Quarter of a second cool down time
+let coolDown = 0;
+
+let started = false;
+
 function setup() {
   // put setup code here
+  createCanvas(width, height);
+  frameRate(fps);
+  // noCursor();
 }
 
 function draw() {
   // put drawing code here
+  background(100, 180, 255);
+
+  player.show();
+  // console.log(coolDown);
+  if (started) {
+    player.update();
+    // console.log(coolDown);
+    if (coolDown > 0) {
+      if (coolDown - 1 < 0) {
+        coolDown = 0;
+      } else {
+        coolDown -= 1;
+      }
+    }
+  }
 }
 
-class Player {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-
-    this.xVel = panSpeed;
-    this.yVel = g;
+function keyPressed() {
+  // Get player input
+  if (keyCode === 32) {
+    if (!started) {
+      // Start the game after the first time space is pressed
+      started = true;
+    }
+    if (coolDown === 0) {
+      // Apply an upward force to the player and reset the coolDown
+      player.yVel = -7;
+      coolDown = coolDownTime;
+      console.log("Jump!");
+      console.log(player.yVel);
+    }
   }
-
-  show() {
-    ellipse(this.x, this.y, 15);
-  }
-
-  update() {}
-}
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
-bird = new Player(50, 50);
-
-while (!gameOver) {
-  bird.show();
+  return false;
 }
